8858acde587ce78393a92173d908c721dda7c555
Casper Groenenberg <c.groenenberg@hotmail.com>
2018-10-07 11:41:02 +0200

    Update app/components/Languages.js
    Refactor using axios

--- app/components/Languages.js
+++ app/components/Languages.js
@@ -6,47 +6,19 @@ import { ScrollView, Text, View, FlatList, Button } from 'react-native';
 export default class Languages extends Component {
   state = {
     data: []
-    // data: [{'key': 'a'}, {'key': 'b'}, {'key': 'c'}]
-    // data: [
-    //   {"model":"Sentra", "doors":4},
-    //   {"model":"Maxima", "doors":4},
-    //   {"model":"Skyline", "doors":2}
-    // ]
   }
 
-  // componentDidMount() {
-  //   axios.get('http://localhost:5000/languages')
-  //     .then(function (response) {
-  //       console.log("Componentdidmount:", response);
-  //     })
-  //     .catch(function (error) {
-  //       console.log("componentdidmount error: ",error);
-  //   });
-  // }
-
-  // componentWillMount() {
-  //   try {
-  //     let response = await fetch(
-  //       'http://localhost:5000/languages'
-  //     );
-  //     let responseJson = await response.json();
-  //     console.log(responseJson)
-  //   } catch (error) {
-  //     console.error(error);
-  //     }
-  //   }
-  // }
-
-  //Make backend call
-  // async componentWillMount() {
-  //   console.log("calling will mount")
-  //   const res = await axios.get('http://localhost:5000/languages');
-  //   console.log("GOT:", res)
-  //   this.setState({ data: [...res] });
-  // }
+  async componentWillMount() {
+    try {
+      //Generate new ngrok link on every restart using: ngrok http 5000
+      let res = await axios.get('http://fe030a79.ngrok.io/languages');
+      this.setState({ data: res.data });
+      } catch (error) {
+        console.error(error);
+    }
+  }
 
   renderItem({item}) {
-    console.log(item)
         return  (
           <View>
              <Text>{item.model}</Text>
@@ -54,22 +26,6 @@ export default class Languages extends Component {
         )
   }
 
-  async onPress() {
-    console.log("pressed button for SW api");
-    try {
-      //Generate new ngrok link on every restart using: ngrok http 5000
-      let response = await fetch('http://fe030a79.ngrok.io/languages');
-      let responseJson = await response.json();
-      console.log("got:", responseJson);
-      this.setState({ data: responseJson });
-      console.log("current state");
-      console.log(this.state.data);
-      // console.log(responseJson);
-      } catch (error) {
-      console.error(error);
-      }
-    }
-  
 
   render() {
     return (
@@ -77,10 +33,6 @@ export default class Languages extends Component {
         <FlatList
           data={this.state.data}
           renderItem={this.renderItem}
-        />
-        <Button
-          title="get data"
-          onPress={this.onPress.bind(this)}
         />
       </View>
     )
